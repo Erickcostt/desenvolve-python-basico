a15fdd3adcee737444701f83b5eb96487a7f06c8
Erickcostt <erickfaria.pdbd236@pdbomdespacho.com.br>
2025-10-16 15:25:12 -0300

    Add files via upload

---
+++ Módulo3/aula2modulo3_questao3.py
@@ -0,0 +1,28 @@
+# - Você está desenvolvendo um sistema de admissão para um clube juvenil de jogos 
+# de tabuleiro. Escreva um programa em Python que pergunte ao usuário sua idade, se já jogou pelo
+#  menos 3 jogos de tabuleiro (resposta deve ser True ou False) e quantas vezes venceu um jogo.
+#  O programa deve imprimir True se o participante tiver entre 16 e 18 anos, já tiver jogado pelo 
+# menos 3 jogos e já ter vencido pelo menos 1 jogo, permitindo seu ingresso no clube. Sua expressão 
+# deve imprimir False caso contrário. Aqui está um exemplo de interação com seu código no terminal, 
+# com entradas de dados destacadas em laranja e as impressões de seu código em branco.
+
+#Terminal
+
+#Digite sua idade: 17
+#Já jogou pelo menos 3 jogos de tabuleiro? True
+#Quantos jogos já venceu? 2
+#Apto para ingressar no clube de jogos de tabuleiro: True
+
+
+idade=int(input("Digite sua idade:"))
+experiencia=input("Já jogou pelo menos 3 jogos de tabuleiro?(True/False)")
+print(experiencia=="sim")
+vitorias=int(input("Quantos jogos já venceu?"))
+
+experiencia=experiencia==True
+
+
+apto = (idade >= 16 and idade <= 18) and experiencia and (vitorias >= 1)
+
+
+print("Apto para ingressar no clube de jogos de tabuleiro:", apto)
---
+++ Módulo3/aula4modulo3_questao2.py
@@ -0,0 +1,12 @@
+nota=int(input("Digite uma avaliação de 1 a 5 para o filme:"))
+
+if nota==5:
+    print("Excelente!")
+if nota==4:
+    print("Muito bom!")
+if nota==3:
+    print("Bom!")
+if nota==2:
+    print("Regular.")
+if nota==1:
+    print("Ruim.")
